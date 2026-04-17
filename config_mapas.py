e7b8e35fe1200c19b4bdd237ccb051810889b756
Josué Peixe <josue.peixe308@gmail.com>
2026-04-17 14:57:56 -0300

    Add files via upload

---
+++ config_mapas.py
@@ -0,0 +1,38 @@
+# config_mapas.py
+# Aqui ficam guardados todos os links do Datawrapper do seu projeto.
+
+LINKS_MAPAS = {
+    2022: {
+        "Brasil": {
+            "Violência (Homicídios)": "SEU_LINK_AQUI",
+            "Gestão Pública (IGMA)": {
+                "IGMA Geral": "SEU_LINK_AQUI",
+                "Desenvolvimento Socioeconômico e Ordem Pública": "SEU_LINK_AQUI"
+            }
+        },
+        "CE": {
+            "Violência (Homicídios)": "https://datawrapper.dwcdn.net/9xAdE/1/",
+            "Gestão Pública (IGMA)": {
+                "IGMA Geral": "https://datawrapper.dwcdn.net/2oJ6N/3/",
+                "Desenvolvimento Socioeconômico e Ordem Pública": "SEU_LINK_AQUI"
+            }
+        }
+        # Pode ir adicionando outros estados aqui depois...
+    },
+    2023: {
+        "Brasil": {
+            "Violência (Homicídios)": "SEU_LINK_AQUI",
+            "Gestão Pública (IGMA)": {
+                "IGMA Geral": "SEU_LINK_AQUI",
+                "Desenvolvimento Socioeconômico e Ordem Pública": "SEU_LINK_AQUI"
+            }
+        },
+        "CE": {
+            "Violência (Homicídios)": "SEU_LINK_AQUI",
+            "Gestão Pública (IGMA)": {
+                "IGMA Geral": "SEU_LINK_AQUI",
+                "Desenvolvimento Socioeconômico e Ordem Pública": "SEU_LINK_AQUI"
+            }
+        }
+    }
+}
