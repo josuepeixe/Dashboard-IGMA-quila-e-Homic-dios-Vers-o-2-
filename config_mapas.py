e536c4062d44e5522980e8e76817a1cded85c0d8
Josué Peixe <josue.peixe308@gmail.com>
2026-04-20 23:21:04 -0300

    config_mapas.py

--- config_mapas.py
+++ config_mapas.py
@@ -1,6 +1,7 @@
 # config_mapas.py
 # Aqui ficam guardados todos os links do Datawrapper do seu projeto.
 
+
 LINKS_MAPAS = {
     2022: {
         "Brasil": {
@@ -14,7 +15,13 @@ LINKS_MAPAS = {
             "Violência (Homicídios)": "https://datawrapper.dwcdn.net/9xAdE/1/",
             "Gestão Pública (IGMA)": {
                 "IGMA Geral": "https://datawrapper.dwcdn.net/2oJ6N/3/",
-                "Desenvolvimento Socioeconômico e Ordem Pública": "SEU_LINK_AQUI"
+                "Desenvolvimento Socioeconômico e Ordem Pública": "https://datawrapper.dwcdn.net/IuBLd/2/",
+                "'Governança, Eficiência Fiscal e Transparência': "https://datawrapper.dwcdn.net/O6veq/2/",
+                "Educação":"https://datawrapper.dwcdn.net/jNOY7/2/",
+                "Saúde e Bem-Estar": "https://datawrapper.dwcdn.net/oS9uU/1/",
+                "Infraestrutura e Mobilidade Urbana":"https://datawrapper.dwcdn.net/zstE3/2/",
+                "Sustentabilidade":"https://datawrapper.dwcdn.net/m20rr/2/"
+                
             }
         }
         # Pode ir adicionando outros estados aqui depois...
