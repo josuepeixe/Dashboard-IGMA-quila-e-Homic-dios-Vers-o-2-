31f760b80963abdc167a7d369bc55d0f6eb41e20
Josué Peixe <josue.peixe308@gmail.com>
2026-04-20 23:27:04 -0300

    config_mapas.py

--- config_mapas.py
+++ config_mapas.py
@@ -16,14 +16,14 @@ LINKS_MAPAS = {
             "Gestão Pública (IGMA)": {
                 "IGMA Geral": "https://datawrapper.dwcdn.net/2oJ6N/3/",
                 "Desenvolvimento Socioeconômico e Ordem Pública": "https://datawrapper.dwcdn.net/IuBLd/2/",
-                "'Governança, Eficiência Fiscal e Transparência': "https://datawrapper.dwcdn.net/O6veq/2/",
-                "Educação":"https://datawrapper.dwcdn.net/jNOY7/2/",
+                "Governança, Eficiência Fiscal e Transparência": "https://datawrapper.dwcdn.net/O6veq/2/",
+                "Educação": "https://datawrapper.dwcdn.net/jNOY7/2/",
                 "Saúde e Bem-Estar": "https://datawrapper.dwcdn.net/oS9uU/1/",
-                "Infraestrutura e Mobilidade Urbana":"https://datawrapper.dwcdn.net/zstE3/2/",
-                "Sustentabilidade":"https://datawrapper.dwcdn.net/m20rr/2/"
-                
+                "Infraestrutura e Mobilidade Urbana": "https://datawrapper.dwcdn.net/zstE3/2/",
+                "Sustentabilidade": "https://datawrapper.dwcdn.net/m20rr/2/"
             }
         }
+    }
         # Pode ir adicionando outros estados aqui depois...
     },
     2023: {
